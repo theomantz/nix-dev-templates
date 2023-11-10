df77cf6216690e8f854c94744ecf504bf5af4f09
theomantz <theo@mantz.nyc>
2023-11-09 19:49:09 -0500

    update flake

--- python/flake.nix
+++ python/flake.nix
@@ -4,9 +4,13 @@
     inputs = {
         nixpkgs = {url = "github:NixOS/nixpkgs/nixos-unstable"; };
         flake-utils = {url = "github:numtide/flake-utils"; };
+        poetry2nix = {
+            url = "github:nix-community/poetry2nix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = { self, nixpkgs, flake-utils, ... }:
+    outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }:
         flake-utils.lib.eachDefaultSystem (system: 
             let 
                 pkgs = import nixpkgs { inherit system; };
